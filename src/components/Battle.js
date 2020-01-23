d2d25af49d800a24c8b0ebd80e24a4409542a6d9
Emily Taylor <emilytaylor@live.com.au>
2020-01-23 21:52:28 +1000

    9. Battle Instructions

---
+++ src/components/Battle.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Instructions from './Instructions'
+
+export default class Battle extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Instructions />
+      </React.Fragment>
+    )
+  }
+}
