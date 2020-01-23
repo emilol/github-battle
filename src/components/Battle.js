1ff15d7f5c5a9e97c125b3a82fc8a07d00024217
Emily Taylor <emilytaylor@live.com.au>
2020-01-23 23:17:04 +1000

    12. Player Preview

--- src/components/Battle.js
+++ src/components/Battle.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Instructions from './Instructions'
 import PlayerInput from './PlayerInput'
+import PlayerPreview from './PlayerPreview'
 
 export default class Battle extends React.Component {
   constructor(props) {
@@ -12,11 +13,18 @@ export default class Battle extends React.Component {
     }
 
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleReset = this.handleReset.bind(this)
   }
 
   handleSubmit(id, player) {
     this.setState({
       [id]: player
+    })
+  }
+
+  handleReset(id) {
+    this.setState({
+      [id]: null
     })
   }
 
@@ -29,17 +37,25 @@ export default class Battle extends React.Component {
         <div className='players-container'>
           <h1 className='center-text header-lg'>Players</h1>
           <div className='row space-around'>
-            { playerOne == null && (
-              <PlayerInput 
-                label='Player One' 
-                onSubmit={(player => { this.handleSubmit('playerOne', player) })}/>
-            )}
+            { playerOne === null
+              ? <PlayerInput 
+                  label='Player One' 
+                  onSubmit={player => { this.handleSubmit('playerOne', player) }}/>
+              : <PlayerPreview
+                  username={playerOne}
+                  label='Player One'
+                  onReset={() => { this.handleReset('playerOne') }} /> 
+            }
             
-            { playerTwo == null && (
-              <PlayerInput 
-                label='Player Two' 
-                onSubmit={(player => { this.handleSubmit('playerTwo', player) })}/>
-            )}
+            { playerTwo === null 
+              ? <PlayerInput 
+                  label='Player Two' 
+                  onSubmit={player => { this.handleSubmit('playerTwo', player) }}/>
+              : <PlayerPreview
+                  username={playerTwo}
+                  label='Player Two'
+                  onReset={() => { this.handleReset('playerTwo') }} /> 
+            }
           </div>
         </div>
       </React.Fragment>
