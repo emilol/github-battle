01aa0561ccd2d311cb22aa9e89158436da2d6b6b
Emily Taylor <emilytaylor@live.com.au>
2020-01-24 14:58:43 +1000

    24. Theme Provider

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import React from 'react';
 import './App.css';
 import Popular from './components/Popular'
 import Battle from './components/Battle';
+import { ThemeProvider } from './contexts/theme';
 
-function App() {
-  return (
-    <div className='container'>
-      <Battle />
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === 'light' ? 'dark' : 'light'
+        }))
+      }
+    }
+  }
+
+  render() {
+    return (
+      <ThemeProvider value={this.state}>
+        <div className='container'>
+          <Battle />
+        </div>
+      </ThemeProvider>
+    )
+  }
 }
-
-export default App;
