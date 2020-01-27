e042c555cf6d224352f45c999437e54a76b99445
Emily Taylor <emilytaylor@live.com.au>
2020-01-27 15:01:47 +1000

    9. Results

--- src/components/Results.js
+++ src/components/Results.js
@@ -6,75 +6,92 @@ import Loading from './Loading'
 import queryString from 'query-string'
 import { Link } from 'react-router-dom'
 
-export default class Results extends React.Component {
-  state = {
-    winner: null,
-    loser: null,
-    error: null,
-    loading: true,
+function battleReducer(state, action) {
+  if (action.type === 'fetch') {
+    return {
+      ...state,
+      loading: true
+    }
+  } else if (action.type === 'success') {
+    return {
+      winner: action.winner,
+      loser: action.loser,
+      error: null,
+      loading: false
+    }
+  } else if (action.type === 'error') {
+    return {
+      ...state,
+      error: 'Error fetching data. Try again',
+      loading: false
+    }
+  } else {
+    throw new Error(`That action type isn't supported.`)
   }
+}
 
-  componentDidMount() {
-    const { playerOne, playerTwo} = queryString.parse(this.props.location.search)
+function useFetch(playerOne, playerTwo) {
+  const [state, dispatch] = React.useReducer(
+    battleReducer, 
+    { winner: null, loser: null, error: null, loading: true }
+  )
+
+  React.useEffect(() => {
+    dispatch({ type: 'fetch' })
 
     battle([playerOne, playerTwo])
-      .then((players) => {
-        this.setState({
-          winner: players[0],
-          loser: players[1],
-          error: null,
-          loading: false,
-        })
+      .then((players) => dispatch({ type: 'success', winner: players[0], loser: players[1] }))
+      .catch((error) => {
+        console.warn('Error fetching results:', error)
+        dispatch({ type: 'error'})
       })
-      .catch(({ message }) => {
-        this.setState({
-          error: message,
-          loading: false
-        })
-      })
+
+  }, [playerOne, playerTwo])
+
+  return state
+}
+
+export default function Results({ location }) {
+  const { playerOne, playerTwo} = queryString.parse(location.search)
+  const { winner, loser, error, loading } = useFetch(playerOne, playerTwo)
+
+  if (loading === true) {
+    return <Loading text='Battling' />
   }
 
-  render() {
-    const { winner, loser, error, loading } = this.state
-
-    if (loading === true) {
-      return <Loading text='Battling' />
-    }
-
-    if (error) {
-      return (
-        <p className='center-text error'>{error}</p>
-      )
-    }
-
+  if (error) {
     return (
-      <React.Fragment>
-        <div className='grid space-around container-sm'>
-          <Card 
-            header={winner.score === loser.score ? 'Tie' : 'Winner' }
-            subheader={`Score: ${winner.score.toLocaleString()}`}
-            avatar={winner.profile.avatar_url}
-            href={winner.profile.html_url}
-            name={winner.profile.login}
-          >
-            <ProfileList profile={winner.profile} />
-          </Card>
-          <Card 
-            header={winner.score === loser.score ? 'Tie' : 'Loser' }
-            subheader={`Score: ${loser.score.toLocaleString()}`}
-            avatar={loser.profile.avatar_url}
-            href={loser.profile.html_url}
-            name={loser.profile.login}
-          >
-            <ProfileList profile={loser.profile} />
-          </Card>
-        </div>
-        <Link
-          to='/battle'
-          className='btn dark-btn btn-space'>
-            Reset
-        </Link>
-      </React.Fragment>
+      <p className='center-text error'>{error}</p>
     )
   }
+
+  return (
+    <React.Fragment>
+      <div className='grid space-around container-sm'>
+        <Card 
+          header={winner.score === loser.score ? 'Tie' : 'Winner' }
+          subheader={`Score: ${winner.score.toLocaleString()}`}
+          avatar={winner.profile.avatar_url}
+          href={winner.profile.html_url}
+          name={winner.profile.login}
+        >
+          <ProfileList profile={winner.profile} />
+        </Card>
+        <Card 
+          header={winner.score === loser.score ? 'Tie' : 'Loser' }
+          subheader={`Score: ${loser.score.toLocaleString()}`}
+          avatar={loser.profile.avatar_url}
+          href={loser.profile.html_url}
+          name={loser.profile.login}
+        >
+          <ProfileList profile={loser.profile} />
+        </Card>
+      </div>
+      <Link
+        to='/battle'
+        className='btn dark-btn btn-space'>
+          Reset
+      </Link>
+    </React.Fragment>
+  )
 }
